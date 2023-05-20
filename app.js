aa5e74f36ed202de9c5c77ec595156fdab4ffad7
joe <joe@local>
2023-05-20 20:13:08 +0100

    log all http requests

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const app = express()
+const morgan = require("morgan")
 const port = process.env.PORT || 3001
+
+app.use(morgan('dev'))
 
 app.get("/", (req, res) => {
     res.type('html').send('hello world')
